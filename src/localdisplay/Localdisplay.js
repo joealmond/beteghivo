c2ad7dda102e090f957455d16a1bb78156906885
joealmond <jozsef.mandula@gmail.com>
2023-01-23 03:21:36 +0100

    Main and Local diplays working

--- src/localdisplay/Localdisplay.js
+++ src/localdisplay/Localdisplay.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 // TODO: a szobába behívott sorszám kijelzése
 // EXAMPLE SCHEMA:
 // {
@@ -6,12 +7,37 @@
 //   "behívasIdeje": "2023-01-20T09:54:24.803Z"
 // }
 export default function Localdisplay({ roomsData }) {
+  const [allCueDataState, setAllCueDataState] = useState({});
+  useEffect(() => {
+    async function getAllCue() {
+      const response = await fetch(`/behivas/${roomsData.szam}`);
+      const allCueData = await response.json();
+      setAllCueDataState(allCueData);
+    }
+
+    setInterval(() => {
+      getAllCue();
+    }, 3000);
+  }, []);
+
   return (
     <section>
       <h2>Helység kijelző</h2>
       <div>
         <div>
           <h3>Rendelő {roomsData.szam + ".  - " + roomsData.megnevezes}</h3>
+        </div>
+        <div>
+          <div>
+            <h3>Sorszám: {allCueDataState?.sorszam}</h3>
+            <h3>Helység: {allCueDataState?.szoba}</h3>
+            <h3>
+              Behívás időpontja:{" "}
+              {new Date(
+                Date.parse(allCueDataState?.behívasIdeje)
+              ).toLocaleTimeString("hu-HU")}
+            </h3>
+          </div>
         </div>
       </div>
     </section>
--- src/maindisplay/Maindisplay.js
+++ src/maindisplay/Maindisplay.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 // TODO: az összes behívott sorszám kijelzése
 // EXAMPLE SCHEMA:
 // [
@@ -8,21 +9,42 @@
 //   }
 // ]
 export default function Maindisplay() {
+  const [allCueDataState, setAllCueDataState] = useState({});
+  useEffect(() => {
+    async function getAllCue() {
+      const response = await fetch("/behivas");
+      const allCueData = await response.json();
+      setAllCueDataState(allCueData);
+    }
+
+    setInterval(() => {
+      getAllCue();
+    }, 3000);
+  }, []);
+
   return (
     <section>
       <h2>Központi kijelző</h2>
       <div>
         <div>
-          <h3>Sorszám</h3>
-          <h3>Helység</h3>
+          <h3>Sorszám: {allCueDataState[0]?.sorszam}</h3>
+          <h3>Helység: {allCueDataState[0]?.szoba}</h3>
+          <h3>
+            Behívás időpontja:{" "}
+            {new Date(
+              Date.parse(allCueDataState[0]?.behívasIdeje)
+            ).toLocaleTimeString("hu-HU")}
+          </h3>
         </div>
         <div>
-          <p>---</p>
-          <p>-</p>
-        </div>
-        <div>
-          <p>___</p>
-          <p>_</p>
+          <p>Sorszám: {allCueDataState[1]?.sorszam}</p>
+          <p>Helység: {allCueDataState[1]?.szoba}</p>
+          <p>
+            Behívás időpontja:{" "}
+            {new Date(
+              Date.parse(allCueDataState[1]?.behívasIdeje)
+            ).toLocaleTimeString("hu-HU")}
+          </p>
         </div>
       </div>
     </section>
