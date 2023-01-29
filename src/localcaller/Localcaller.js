d1594fbcbcc273cf56cdad73fc0dfb6b41a46b49
joealmond <jozsef.mandula@gmail.com>
2023-01-29 17:42:53 +0100

    Calling now controlled by server in Localcaller

--- src/localcaller/Localcaller.js
+++ src/localcaller/Localcaller.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import uuid from "react-uuid";
 
 // TODO: javítani a táblázat formátumát, adatait
@@ -8,112 +8,64 @@ import uuid from "react-uuid";
 // TODO: csak az adatokkal dolgozni és abból renderelni az elemekt
 
 export default function Localcaller({ roomsData, roomId }) {
-  const [renderRenderRowState, setRenderRenderRowState] = useState();
+  const [rowState, setRowState] = useState([]);
   const [renderButtonState, setRenderButtonState] = useState();
 
-  const cueRefData1 = useRef([]);
-  const cueRefData2 = useRef([]);
-  const cueRefData3 = useRef([]);
+  function TableRow(roomId) {
+    useEffect(() => {
+      async function getRoomCue() {
+        const response = await fetch(`/sorszam/${roomId}`);
+        const roomCueData = await response.json();
+        if (roomCueData[0]) {
+          setRowState(roomCueData);
+        } else {
+          setRowState([
+            {
+              sorszam: "---",
+              vizsgalatKod: "---",
+              taj: "-------",
+              erkezesIdeje: "----",
+              varakozok: "--",
+            },
+          ]);
+        }
+      }
+      setInterval(() => {
+        // TODO: add clearinterval
+        getRoomCue();
+      }, 10000);
+    }, []);
 
-  const cueRef1 = useRef([]);
-  const cueRef2 = useRef([]);
-  const cueRef3 = useRef([]);
-  const cueNumRef = useRef(localStorage.getItem("cueNumber"));
-
-  let cueArray = {};
-
-  for (let i = 0; i < roomsData.length; i++) {
-    cueArray[i] = [];
+    return (
+      <>
+        {rowState.map((row, i) => (
+          <tr key={uuid()}>
+            <td>{row.sorszam}</td>
+            <td>{row.vizsgalatKod}</td>
+            <td>{row.erkezesIdeje}</td>
+            <td>{row.taj}</td>
+            <td>{row.varakozok}</td>
+          </tr>
+        ))}
+      </>
+    );
   }
-
-  let cueArrayData = {};
-
-  for (let i = 0; i < roomsData.length; i++) {
-    cueArrayData[i] = [];
-  }
-
-  let tajTemp = "-";
-
-  function RenderRow(roomId) {
-    if (
-      roomId === Number(localStorage.getItem("room")) &&
-      localStorage.getItem("cueNumber") !== cueNumRef.current
-    ) {
-      cueArray[roomId - 1] = (
-        <tr key={uuid()}>
-          <td>{localStorage.getItem("cueNumber")}</td>
-          <td>{roomsData.megnevezes}</td>
-          <td>{localStorage.getItem("cueInTime")}</td>
-          <td>{tajTemp}</td>
-          <td>{roomId}</td>
-          <td>{localStorage.getItem("room")}</td>
-        </tr>
-      );
-
-      cueArrayData[roomId - 1] = {
-        cueNumber: localStorage.getItem("cueNumber"),
-        room: localStorage.getItem("room"),
-        roomId: roomId,
-      };
-      cueRefData1.current = [...cueRefData1.current, cueArrayData[0]];
-      cueRefData2.current = [...cueRefData2.current, cueArrayData[1]];
-      cueRefData3.current = [...cueRefData3.current, cueArrayData[2]];
-
-      cueNumRef.current = localStorage.getItem("cueNumber");
-      cueRef1.current = [...cueRef1.current, cueArray[0]];
-      cueRef2.current = [...cueRef2.current, cueArray[1]];
-      cueRef3.current = [...cueRef3.current, cueArray[2]];
-    }
-    if (roomId === 1) return cueRef1.current;
-    if (roomId === 2) return cueRef2.current;
-    if (roomId === 3) return cueRef3.current;
-  }
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRenderRenderRowState(RenderRow(roomId));
-    }, 10000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    setRenderButtonState(Button(roomId));
-    // TODO: resolve: "Warning: Maximum update depht exceeded..."
-  }, [renderButtonState]);
 
   function Button(roomId) {
-    let jsonData = {};
-    let renderData = {};
-    if (roomId === 1) {
-      jsonData.current = cueRefData1.current;
-      renderData.current = cueRef1.current;
-    } else if (roomId === 2) {
-      jsonData.current = cueRefData2.current;
-      renderData.current = cueRef2.current;
-    } else if (roomId === 3) {
-      jsonData.current = cueRefData3.current;
-      renderData.current = cueRef3.current;
-    } else {
-      // TODO: handle error
-      console.log("no matching room");
-    }
     async function callNext() {
       const response = await fetch(`/behivas/${roomId}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(jsonData.current[0].cueNumber),
+        body: JSON.stringify(rowState[0].sorszam),
       });
+      // TODO: need this?
       const callData = await response.json();
-      jsonData.current.shift();
-      renderData.current.shift();
     }
 
     return <button onClick={callNext}>Kérem a következőt</button>;
   }
-
   return (
     <section>
       <h2>Behívó</h2>
@@ -131,9 +83,9 @@ export default function Localcaller({ roomsData, roomId }) {
               <th>Szoba</th>
             </tr>
           </thead>
-          <tbody>{renderRenderRowState}</tbody>
+          <tbody>{TableRow(roomId)}</tbody>
         </table>
-        {renderButtonState}
+        {Button(roomId)}
       </div>
     </section>
   );
